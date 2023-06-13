63ab3fbdb4e9abef87abe89c2bcb508b5a9c8647
faaizhr <faizhafizh28@gmail.com>
2023-06-13 15:56:26 +0700

    update kode pemesanan pakaian

--- src/component/Pemesanan/pemesanan.js
+++ src/component/Pemesanan/pemesanan.js
@@ -331,7 +331,7 @@ const Pemesanan = () => {
 
   var date = new Date()
   var day = ("0" + date.getDate()).slice(-2)
-  var month = ("0" + date.getMonth()).slice(-2)
+  var month = ("0" + (date.getMonth() + 1)).slice(-2)
   var year = date.getFullYear()
   
   var fulltime = year + month + day
